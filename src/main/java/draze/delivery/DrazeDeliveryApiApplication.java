c1f01747ecf195f0164d2924c733e36264d07df4
Caio <caiohbda777@gmail.com>
2025-03-18 14:43:53 -0300

    initial commit

---
+++ src/main/java/draze/delivery/DrazeDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package draze.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrazeDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrazeDeliveryApiApplication.class, args);
+	}
+
+}
