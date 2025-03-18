36c8df978a18ffd5a6d8d769d90bab00282d1c33
Caio <caiohbda777@gmail.com>
2025-03-18 15:42:31 -0300

    feature: implements domain layer

---
+++ src/main/java/draze/delivery/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package draze.delivery.domain.repository;
+
+import draze.delivery.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository{
+}
